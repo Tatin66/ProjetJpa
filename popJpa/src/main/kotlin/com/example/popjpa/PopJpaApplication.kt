c3164e226f0686e660625689ac4a30fa5e184336
Manonrtnt <70939460+Manonrtnt@users.noreply.github.com>
2023-07-18 09:05:07 +0200

    Init project

---
+++ popJpa/src/main/kotlin/com/example/popjpa/PopJpaApplication.kt
@@ -0,0 +1,11 @@
+package com.example.popjpa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PopJpaApplication
+
+fun main(args: Array<String>) {
+	runApplication<PopJpaApplication>(*args)
+}
---
+++ popJpa/src/test/kotlin/com/example/popjpa/PopJpaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.popjpa
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PopJpaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
