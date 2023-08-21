9009d3d6ecc13832b81c6964e64460ea497ed070
valentin <val.tatin@orange.fr>
2023-08-21 17:10:04 +0200

    my message

--- popJpa/src/main/kotlin/com/example/popjpa/CommandeController.kt
+++ popJpa/src/main/kotlin/com/example/popjpa/CommandeController.kt
@@ -1,18 +1,23 @@
 package com.example.popjpa
 
+import com.example.popjpa.entity.ProduitEntity
 import com.example.popjpa.repository.ProduitRepository
 import com.example.popjpa.service.CommandeService
+import com.example.popjpa.service.ProduitService
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.thymeleaf.util.StringUtils.append
 
 @Controller
-class CommandeController(val commandeService: CommandeService) {
+class CommandeController(val produitService: ProduitService) {
 
     @GetMapping("/commande")
-    fun commandes(): String{
-        println("commande")
-        val commande = commandeService.getCommande()
-        println(commande)
+    fun commandes(model: Model): String{
+        val produitListe = produitService.getProduit()
+
+        println(produitListe)
+        model.addAttribute("productList", produitListe)
         return "commande"
     }
 
